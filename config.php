de6851a574d82fe879fa83b029083b1ba9aa32fc
Yaseen k <yzac1569@gmail.com>
2025-05-16 14:06:51 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
 // MongoDB connection configuration
-define('MONGODB_URI', 'mongodb+srv://yaseen:3GWpYAbQuZP06S6N@billcluster.rhevn0n.mongodb.net/?retryWrites=true&w=majority&appName=BillCluster');
+define('mongodb_uri', 'mongodb+srv://yaseen:3GWpYAbQuZP06S6N@billcluster.rhevn0n.mongodb.net/?retryWrites=true&w=majority&appName=BillCluster');
 ?>
