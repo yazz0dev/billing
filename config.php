8f8b883fddbec675c7d515a5b4369fb827ff9dc9
Yaseen k <yzac1569@gmail.com>
2025-05-18 00:18:44 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,4 +1,8 @@
 <?php
 // MongoDB connection configuration
-define('mongodb_uri', 'mongodb+srv://yaseen:3GWpYAbQuZP06S6N@billcluster.rhevn0n.mongodb.net/?retryWrites=true&w=majority&appName=BillCluster');
+// This constant is primarily for local development if .env or getenv('MONGODB_URI') is not used.
+// On Vercel, getenv('MONGODB_URI') will be used.
+if (!defined('MONGODB_URI_CONFIG')) {
+    define('MONGODB_URI_CONFIG', 'mongodb+srv://yaseen:3GWpYAbQuZP06S6N@billcluster.rhevn0n.mongodb.net/?retryWrites=true&w=majority&appName=BillCluster');
+}
 ?>
