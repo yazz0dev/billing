518fd43a39709c7495f0760b716f4305c8bdf637
Yaseen k <yzac1569@gmail.com>
2025-05-18 11:04:13 +0530

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,13 @@
+<?php // config/database.php
+
+return [
+    'mongodb' => [
+        'uri' => $_ENV['MONGODB_URI'] ?? 'mongodb://localhost:27017',
+        'database_name' => 'billing_refactored', // Choose your DB name
+        'options' => [], // MongoDB URI options
+        'driver_options' => [ // MongoDB driver options
+             'serverSelectionTimeoutMS' => 5000,
+             'connectTimeoutMS' => 10000,
+        ],
+    ],
+];
