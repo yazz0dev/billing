7c83c3f36a3d4dd706c04a73b8d18fc913cf036a
Yaseen k <yzac1569@gmail.com>
2025-05-18 11:03:38 +0530

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,1 +1,9 @@
+<?php // config/app.php
 
+return [
+    'name' => 'Supermarket Billing System',
+    'env' => $_ENV['APP_ENV'] ?? 'production',
+    'debug' => filter_var($_ENV['DEBUG_MODE'] ?? false, FILTER_VALIDATE_BOOLEAN),
+    'url' => $_ENV['APP_URL'] ?? 'http://localhost',
+    'session_name' => 'BILLING_SESSION',
+];
