4a19be597d925aaab30161afeb64d1ddf409d22e
Yaseen k <yzac1569@gmail.com>
2025-05-19 09:58:04 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,18 +1,22 @@
-// In config/database.php
+<?php
 
-'default' => env('DB_CONNECTION', 'mongodb'),
+use Illuminate\Support\Str;
 
-'connections' => [
-    // ... other SQL examples (can be removed if not used) ...
-
-    'mongodb' => [
-        'driver'   => 'mongodb',
-        'dsn'      => env('MONGO_DB_URI'), // Using DSN directly
-        'database' => env('MONGO_DB_DATABASE', 'billing_refactored'),
-        // 'options' => [
-        //     'serverSelectionTimeoutMS' => env('MONGO_SERVER_SELECTION_TIMEOUT_MS', 5000),
-        //     'connectTimeoutMS' => env('MONGO_CONNECT_TIMEOUT_MS', 10000),
-        // ],
-        // 'driver_options' => [],
+return [
+    'default' => env('DB_CONNECTION', 'mongodb'),
+    'connections' => [
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'dsn'      => env('MONGO_DB_URI'),
+            'database' => env('MONGO_DB_DATABASE', 'billing_refactored'),
+            // You can add options directly here if needed, or rely on the DSN
+            // 'options' => [
+            //     'serverSelectionTimeoutMS' => env('MONGO_SERVER_SELECTION_TIMEOUT_MS', 5000),
+            //     'connectTimeoutMS' => env('MONGO_CONNECT_TIMEOUT_MS', 10000),
+            // ],
+        ],
+        // ... other default connections like sqlite, mysql, pgsql, sqlsrv (can be removed if not used)
     ],
-],
+    'migrations' => 'migrations',
+    // ... Redis config ...
+];
